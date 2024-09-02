4b30b737c6f2de230700fb26ddb6f32851ede2b8
michelmoura1989 <124470627+michelmoura1989@users.noreply.github.com>
2024-09-01 22:30:43 -0300

    versao4

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ class Conta:
         self.saldo = 0
         self.transacoes = []
 
-    def depositar(self, valor):
+    def depositar(self, valor, data):
         self.saldo += valor
-        self.transacoes.append({'tipo': 'Depósito', 'valor': valor})
+        self.transacoes.append({'tipo': 'Depósito', 'valor': valor, 'data': data})
 
-    def sacar(self, valor):
+    def sacar(self, valor, data):
         if valor <= self.saldo:
             self.saldo -= valor
-            self.transacoes.append({'tipo': 'Saque', 'valor': -valor})
+            self.transacoes.append({'tipo': 'Saque', 'valor': -valor, 'data': data})
         else:
             print("Saldo insuficiente")
 
 # Exemplo de uso
 conta = Conta("Usuário 1")
-conta.depositar(100)
-conta.sacar(50)
-print(conta.transacoes)  # Transações corretas
+conta.depositar(100, "2024-08-23")
+conta.sacar(50, "2024-08-24")
+print(conta.transacoes)
